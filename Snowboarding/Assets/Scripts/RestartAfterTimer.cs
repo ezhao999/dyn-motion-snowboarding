75410b835b7b09447fff9e6e472ea34f4f0a6dbb
ezhao999 <85084944+ezhao999@users.noreply.github.com>
2023-04-06 02:45:00 -0400

    Wheel Collider Lab
    New scene to explore wheel physics. SnowboardWheel.cs tweaked for editor use.

--- Snowboarding/Assets/Scripts/RestartAfterTimer.cs
+++ Snowboarding/Assets/Scripts/RestartAfterTimer.cs
@@ -6,10 +6,12 @@ public class RestartAfterTimer : MonoBehaviour
 {
     public float restartTime = 1.0f;
     Vector3 startPos;
+    Quaternion startRot;
     // Start is called before the first frame update
     void Start()
     {
         startPos = this.transform.position;
+        startRot = this.transform.rotation;
         StartCoroutine(RestartAfterTime(restartTime));
     }
 
@@ -17,6 +19,7 @@ public class RestartAfterTimer : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         this.transform.position = startPos;
+        this.transform.rotation = startRot;
         StartCoroutine(RestartAfterTime(restartTime));
 
     }
--- Snowboarding/Assets/Scripts/SnowboardWheel.cs
+++ Snowboarding/Assets/Scripts/SnowboardWheel.cs
@@ -9,13 +9,14 @@ public class SnowboardWheel : MonoBehaviour
     [SerializeField]
     private WheelCollider snowboardWheel;
     private inputData _inputData; // for VR controller rotation input
-    private Vector3 inputForceDir = new Vector3(0, 0, 0);
+    private Vector3 inputForceDir = new Vector3(0, 0, 0);   
 
     [SerializeField]
     private GameObject wheelCylinder;
     [SerializeField]
     private Text DebugText;
 
+    public Vector3 currentVisualRotation;
     public float currentWheelRotation = 0;
 
     void Start()
@@ -25,14 +26,18 @@ public class SnowboardWheel : MonoBehaviour
 
     private void Update()
     {
-        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rightQuat))
+/*        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rightQuat))
         {
             Vector3 rightInput = rightQuat.eulerAngles;
             currentWheelRotation = rightInput.z;
 
-        }
-        DebugText.text = currentWheelRotation.ToString();
+        }*/
+        // DebugText.text = currentWheelRotation.ToString();
+        currentVisualRotation = wheelCylinder.transform.localRotation.eulerAngles;
+        // snowboardWheel.steerAngle = currentWheelRotation; // used for controller input first
+        currentWheelRotation = currentVisualRotation.y;
         snowboardWheel.steerAngle = currentWheelRotation;
+
     }
 
     //private void OnTilt()
