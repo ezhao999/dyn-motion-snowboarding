b6ec2484fb2da1a0af34dbdff9fcba8633c04ef4
ezhao999 <85084944+ezhao999@users.noreply.github.com>
2023-04-09 17:53:14 -0400

    Wheel script duplicated

---
+++ Snowboarding/Assets/Scripts/SnowboardVehicle.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+public class SnowboardVehicle : MonoBehaviour
+{
+    [SerializeField]
+    private WheelCollider snowboardWheel;
+    private inputData _inputData; // for VR controller rotation input
+    private Vector3 inputForceDir = new Vector3(0, 0, 0);   
+
+    [SerializeField]
+    private GameObject wheelCylinder;
+    [SerializeField]
+    private Text DebugText;
+
+    public Vector3 currentVisualRotation;
+    public float currentWheelRotation = 0;
+
+    void Start()
+    {
+        _inputData = GetComponent<inputData>(); // for VR controller rotation input
+    }
+
+    private void Update()
+    {
+/*        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rightQuat))
+        {
+            Vector3 rightInput = rightQuat.eulerAngles;
+            currentWheelRotation = rightInput.z;
+
+        }*/
+        // DebugText.text = currentWheelRotation.ToString();
+        currentVisualRotation = wheelCylinder.transform.localRotation.eulerAngles;
+        // snowboardWheel.steerAngle = currentWheelRotation; // used for controller input first
+        currentWheelRotation = currentVisualRotation.y;
+        snowboardWheel.steerAngle = currentWheelRotation;
+
+    }
+
+    //private void OnTilt()
+    //{
+    //    if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rightQuat))
+    //    {
+    //        Vector3 rightInput = rightQuat.eulerAngles;
+    //        currentWheelRotation = rightInput.z;
+            
+    //    }
+    //}
+
+}
