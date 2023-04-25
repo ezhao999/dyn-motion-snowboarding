34db4b92fe53362284ef5399e8e9f7bffacf3a66
ezhao999 <85084944+ezhao999@users.noreply.github.com>
2023-04-25 17:38:24 -0400

    Wind and SFX added

--- Snowboarding/Assets/Scripts/exposedSoundUpdate.cs
+++ Snowboarding/Assets/Scripts/exposedSoundUpdate.cs
@@ -6,34 +6,64 @@ using UnityEngine.Audio;
 public class exposedSoundUpdate : MonoBehaviour
 {
     public AudioMixer boardMixer;
+    public SnowboardVehicle player;
 
-    [Range(0.0f, 50.0f)] public float velocity;
+    //[Range(0.0f, 50.0f)] public float velocity;
+    public float velocity;
+    public float lastVelocity;
+    public float accel;
+
+    // snow sounds
     private float freqMax = 3.0f;
     private float freqMin = 0.5f;
-    // Start is called before the first frame update
+
+    // wind sounds
+    private float pitchMin = 1.0f;
+    private float pitchMax = 6.5f;
+    private float pitchMult = 0.15f;
+
+    // perlin noise
+    float variance = 1.0f;
+    float xScale = 1.0f;
+    float mult = 0.25f;
+    float perlinModifier;
+
     void Start()
     {
-        
+        lastVelocity = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        velocity = player.rb.velocity.magnitude;
+        accel = (velocity - lastVelocity) / Time.fixedDeltaTime;
+        lastVelocity = velocity;
+        perlinModifier = variance * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
         updateEnvSounds();
     }
 
     void updateEnvSounds()
     {
-        float snowGain = scale(0, 50.0f, freqMin, freqMax, velocity);
+        // snow sounds
+        accel = Mathf.Clamp(accel, -5, 0);
+        float snowGain = scale(0, -5, freqMin, freqMax, accel);
         boardMixer.SetFloat("snowSound", snowGain);
+
+        // wind sounds
+        float windPitch = velocity * pitchMult;
+        windPitch = Mathf.Clamp(windPitch, pitchMin, pitchMax);
+        windPitch += windPitch * perlinModifier * mult;
+        boardMixer.SetFloat("windPitch", windPitch);
+
     }
 
     float scale(float a, float b, float c, float d, float oldVal)
     {
         float oldRange = b - a;
         float newRange = d - c;
-        float proportion = oldVal / oldRange;
-        float newVal = proportion * newRange;
+        float proportion = (oldVal - a) / oldRange;
+        float newVal = c + (proportion * newRange);
         return newVal;
     }
 }
