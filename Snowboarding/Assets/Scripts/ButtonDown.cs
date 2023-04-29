c4152209ddffeb4a03416b7ba0a48949eb8cb953
JiaqiWang7 <jiaqiwa2@andrew.cmu.edu>
2023-04-29 18:17:28 -0400

    Added Interactable cutscene

--- Snowboarding/Assets/Scripts/ButtonDown.cs
+++ Snowboarding/Assets/Scripts/ButtonDown.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class ButtonDown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayableDirector PlayableDirector;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Play(){
+        PlayableDirector.Play();
     }
 }
